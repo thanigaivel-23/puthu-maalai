614b67418605650501c69fa1e11445342c34cee0
thanigaivel-23 <23thanigaivel@gmail.com>
2024-01-29 11:50:46 +0530

    new updates

--- backend/app.js
+++ backend/app.js
@@ -15,18 +15,20 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
-
+app.get('/', (req, res) => {
+    res.send('working')
+})
 app.use("/api", products);
 app.use("/api", auth);
 app.use("/api", order);
 app.use("/api", payment);
 
-if (process.env.NODE_ENV == "production") {
-    app.use(express.static(path.join(__dirname, '../front-end/create-react/build')))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../front-end/create-react/build/index.html'))
-    })
-}
+// if (process.env.NODE_ENV == "production") {
+//     app.use(express.static(path.join(__dirname, '../front-end/create-react/build')))
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, '../front-end/create-react/build/index.html'))
+//     })
+// }
 
 app.use(require('./middleware/error'));
 
