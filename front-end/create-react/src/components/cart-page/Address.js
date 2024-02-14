2d5f59d30a9918bda2a7dcf702739a5c9e762609
AV_Prakash <prakash@jobsgaar.com>
2024-02-14 11:09:22 +0530

    code reverted

--- front-end/create-react/src/components/cart-page/Address.js
+++ front-end/create-react/src/components/cart-page/Address.js
@@ -7,8 +7,6 @@ import CheckoutSteps from './CheckoutSteps';
 
 const Address = () => {
 
-
-    console.log("prakash")
     const { shippingInfo = {} } = useSelector(state => state.cartState)
     const dispatch = useDispatch()
     const navigate = useNavigate()
