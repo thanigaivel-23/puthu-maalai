1a62ccd988ee6a7e98a948dd3f5b2ff99af2e0ff
AV_Prakash <prakash@jobsgaar.com>
2024-02-14 10:48:47 +0530

    space added

--- front-end/create-react/src/components/cart-page/Address.js
+++ front-end/create-react/src/components/cart-page/Address.js
@@ -17,6 +17,9 @@ const Address = () => {
     const [state, setState] = useState(shippingInfo.state || "")
     const [postalCode, setPostalCode] = useState(shippingInfo.postalCode || "")
 
+
+
+
     const country = 'India'
 
 
@@ -31,8 +34,8 @@ const Address = () => {
 
     //     const fetchData = async () => {
     //         let { data } = await axios.get(`https://app.zipcodebase.com/api/v1/search?apikey=6ec05f60-c8d2-11ee-84c3-d558530dec22&codes=${postalCode}`)
-         
-         
+
+
     //         setCity(data.results[postalCode][0].province)
     //         setState(data.results[postalCode][0].state)
 
@@ -73,8 +76,8 @@ const Address = () => {
                     <p className='px-2 py-3 text-xs font-bold text-gray-700 bg-gray-100 md:bg-white'>ADDRESS</p>
                     <input type="number" placeholder='Mobile No' value={phoneNo} onChange={(e) => setPhoneNo(e.target.value)} className='outline-none border rounded-sm box-border w-11/12 m-3 px-3 py-2 focus:border-gray-700 placeholder:text-sm' />
                     <input type="text" placeholder='Address (House No, Street, Area)' value={address} onChange={(e) => setAddress(e.target.value)} required className='outline-none border rounded-sm box-border w-11/12 m-3 px-3 py-2 focus:border-gray-700 placeholder:text-sm' />
-                    <input type="text"  placeholder='City / District' value={city} onChange={(e) => setCity(e.target.value)} required className='outline-none border rounded-sm box-border w-11/12 m-3 px-3 py-2 focus:border-gray-700 placeholder:text-sm' />
-                    <input type="text"  placeholder='State' value={state} onChange={(e) => setState(e.target.value)} required className='outline-none border rounded-sm box-border w-11/12 m-3 px-3 py-2 focus:border-gray-700 placeholder:text-sm' />
+                    <input type="text" placeholder='City / District' value={city} onChange={(e) => setCity(e.target.value)} required className='outline-none border rounded-sm box-border w-11/12 m-3 px-3 py-2 focus:border-gray-700 placeholder:text-sm' />
+                    <input type="text" placeholder='State' value={state} onChange={(e) => setState(e.target.value)} required className='outline-none border rounded-sm box-border w-11/12 m-3 px-3 py-2 focus:border-gray-700 placeholder:text-sm' />
                     <input type="number" placeholder='Pin Code' value={postalCode} onChange={(e) => setPostalCode(e.target.value)} required className='outline-none border rounded-sm box-border w-11/12 m-3 px-3 py-2 focus:border-gray-700 placeholder:text-sm' />
                 </div>
 
